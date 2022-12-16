8d7dcb773ed28b35ca3fdeb730fea2aa84769afd
lola martinhao <lola.martinhao@gmail.com>
2022-12-16 14:13:57 -0300

    adding contact form with email redirect service

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,11 @@
 import './index.scss'
-import AnimatedLetters from '../AnimatedLetters';
-import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
 
   useEffect(() => {
     // wrap the async call
@@ -16,6 +18,21 @@ const Contact = () => {
     loadData();
   }, [])
 
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm('service_om2w8mj', 'template_vrjjyao', refForm.current, '9pYbBxJ36tuu3lv7-')
+    .then(
+      () => {
+        alert('Mensagem enviada! Obrigada :)')
+        window.location.reaload(false)
+      },
+      () => {
+        alert('Falha ao enviar, por favor tente novamente')
+      }
+    )
+  }
+
   return (
     <div className='container contact-page'>
       <div className='text-zone'>
@@ -27,8 +44,29 @@ const Contact = () => {
           />
         </h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam faucibus arcu vel diam porttitor fermentum. Curabitur ut dolor non enim rutrum congue. Morbi sagittis tellus eget tincidunt sagittis. Mauris eget nulla dictum, sagittis tellus ac, ornare nunc. Aenean in ligula enim. Nullam erat ex, feugiat et lacus ut, ultrices egestas lectus. Nunc ullamcorper in sem bibendum fermentum. Morbi egestas erat at lectus tristique semper.
+          Preparei esse forms pra facilitar o seu contato! Se preferir, também pode me chamar no <a className="text-link" href="https://www.linkedin.com/in/lola-martinhao/" target="_blank" rel="noopener noreferrer">LinkedIn</a> ♥
         </p>
+        <div className='contact-form'>
+          <form ref={refForm} onSubmit={sendEmail}>
+            <ul>
+              <li className="half">
+                <input type="text" name="name" placeholder="Nome" required />
+              </li>
+              <li className="half">
+                <input type="email" name="email" placeholder="e-mail" required />
+              </li>
+              <li>
+                <input placeholder="Assunto" type="text" name="subject" required />
+              </li>
+              <li>
+                <textarea placeholder="Mensagem" name="message" required></textarea>
+              </li>
+              <li>
+                <input type="submit" className='contact-button' value="ENVIAR" />
+              </li>
+            </ul>
+          </form>
+        </div>
       </div>
     </div>
   )
