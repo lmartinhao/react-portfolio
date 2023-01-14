e6c8ece1b1f778464ac8e0f524ac84e181e3a776
lola martinhao <lola.martinhao@gmail.com>
2023-01-14 12:56:34 -0300

    adjusting projects iteration

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,16 +3,128 @@ import AnimatedLetters from '../AnimatedLetters';
 import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import Vizinapp from './vizinapp';
-import Lewacar from './lewacar';
-import ReactBurger from './ReactBurger';
-import Portfolio from './portfolio';
 import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'
-import Landing from './landing-page';
-import Watch from './watch-list';
-import Pong from './pong';
-import Freeway from './freeway';
-import AluraMidi from './aluramidi';
+import { Project } from './Project';
+
+const projects = [
+  {
+    id: 1,
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg',
+    title: 'Freeway',
+    url: 'https://javascript-freeway.vercel.app/',
+    text: 'Reprodução do jogo Freeway em HTML, JavaScript e CSS',
+    deployed: true,
+    update: 'Finished',
+    tags: [
+      'html', 'css', 'javascript'
+    ],
+    github: 'https://github.com/lmartinhao/javascript-freeway'
+  },
+  {
+    id: 2,
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg',
+    title: 'AluraMIDI',
+    url: 'https://aluramidi-two.vercel.app/',
+    text: 'Teclado em HTML e CSS com sons via JavaScript',
+    deployed: true,
+    update: 'Finished',
+    tags: [
+      'html', 'css', 'javascript'
+    ],
+    github: 'https://github.com/lmartinhao/aluramidi'
+  },
+  {
+    id: 3,
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg',
+    title: 'Landing page',
+    url: 'https://landing-page-sweetestclub.netlify.app/',
+    text: 'Landing page com formulário de cadastro em HTML e CSS',
+    deployed: false,
+    update: 'Finished',
+    tags: [
+      'html', 'css'
+    ],
+    github: 'https://github.com/lmartinhao/sweetest-club-landing-page'
+  },
+  {
+    id: 4,
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-plain.svg',
+    title: 'Le WaCar',
+    url: 'https://react-todo-list-lmartinhao.vercel.app/',
+    text: 'App em Ruby on rails com Devise, Pundit e dados no PostgreSQL',
+    deployed: true,
+    update: 'App Style',
+    tags: [
+      'ruby', 'rails', 'html', 'sass', 'javascrip'
+    ],
+    github: 'https://github.com/lmartinhao/rails-lewacar'
+  },
+  {
+    id: 5,
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+    title: 'React Burger',
+    url: 'https://react-burger-lmartinhao.vercel.app/',
+    text: 'Aplicação fictícia de carrinho de compras',
+    deployed: false,
+    update: 'Componente do carrinho',
+    tags: [
+      'react', 'javascript', 'html', 'css'
+    ],
+    github: 'https://github.com/lmartinhao/react-burger'
+  },
+  {
+    id: 6,
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-plain.svg',
+    title: 'vizinAPP',
+    url: 'https://vizinapp.herokuapp.com/',
+    text: 'App em Ruby on rails com Devise, Active Admin e dados no PostgreSQL',
+    deployed: true,
+    update: 'Ajustar admin style',
+    tags: [
+      'ruby', 'rails', 'html', 'sass', 'javascrip', 'figma'
+    ],
+    github: 'https://github.com/lmartinhao/vizinapp'
+  },
+  {
+    id: 7,
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+    title: 'Ignite Feed',
+    url: 'https://react-feed-page.vercel.app/',
+    text: 'Social feed desenhado no Figma e criado em React com TypeScript',
+    deployed: true,
+    update: 'Finished',
+    tags: [
+      'react', 'typescrip', 'html', 'css module', 'figma'
+    ],
+    github: 'https://github.com/lmartinhao/react-feed-page'
+  },
+  {
+    id: 8,
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+    title: 'Cool ToDo',
+    url: 'https://react-todo-list-lmartinhao.vercel.app/',
+    text: 'Task management desenhado no Figma e criado em React com TypeScript',
+    deployed: true,
+    update: 'Finished',
+    tags: [
+      'react', 'typescrip', 'html', 'css module', 'figma'
+    ],
+    github: 'https://github.com/lmartinhao/react-todo-list'
+  },
+  {
+    id: 9,
+    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
+    title: 'Dream Tasks',
+    url: '',
+    text: 'Task management criado em React com TypeScript com animações',
+    deployed: false,
+    update: 'Coding',
+    tags: [
+      'react', 'typescrip', 'html', 'css module'
+    ],
+    github: 'https://github.com/lmartinhao'
+  },
+]
 
 const Projects = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -54,15 +166,20 @@ const Projects = () => {
       </h1>
       <div className='card-zone'>
         <div className='cards'>
-          <Vizinapp />
-          <Lewacar />
-          <Portfolio />
-          <ReactBurger />
-          <Landing />
-          <AluraMidi />
-          <Freeway />
-          <Pong />
-          <Watch />
+          {projects.slice(0).reverse().map(project => (
+            <Project
+              key={project.id}
+              icon={project.icon}
+              title={project.title}
+              url={project.url}
+              text={project.text}
+              deployed={project.deployed}
+              update={project.update}
+              tags={project.tags}
+              github={project.github}
+            />
+          )
+        )}
         </div>
       </div>
     </div>
